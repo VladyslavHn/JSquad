e20749f790b4c55ba195257004a475ad06bb0a8b
VladyslavHn <vlad.gnatok@gmail.com>
2024-02-22 20:19:50 +0100

    aftermit=firstmarkup

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@
 import './js/ShoppingList/ShoppingList/shoppinglist';
 import './js/ShoppingList/CardPagination/cardpagination';
 import './js/MainPage/Header/header';
-import './js/MainPage/BestSellers/bestsallers';
+import './js/MainPage/BestSellers/bestsellers';
 import './js/MainPage/CategoryPage/categorypage';
 import './js/MainPage/Sidebar/SidebarAllCategories/sidebarallcategories';
 import './js/MainPage/Sidebar/SidebarSupport/sidebarsupport';
-import './js/Servises/api';
-import './js/Servises/helpers';
+import './js/Services/api';
+import './js/Services/helpers';
 import './js/SignUp/signup';
 import './js/SignUp/signupmarkup' 
 import './js/MainPage/ModalWindow/modalwindow' 
