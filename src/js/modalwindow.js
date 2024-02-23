bce570d13b4228fac030bae8f2f477a316d4ea81
NNASTYA2007 <nedyaknast@gmail.com>
2024-02-24 00:31:00 +0100

    Update
    Html and Css

---
+++ src/js/modalwindow.js
@@ -0,0 +1,9 @@
+import backendAPI from './Services/api';
+backendAPI.getBookDescription;
+//div backdrop
+// const backdrop = document.querySelector('.backdrop');
+// function showModal() {
+//   backdrop.classList.add('.is-open');
+// }
+
+// showModal();
