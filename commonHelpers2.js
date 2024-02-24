b39e331d78460bda34d9b037447ecb1ab16019b1
VladyslavHn <141771169+VladyslavHn@users.noreply.github.com>
2024-02-24 18:49:25 +0000

    Deploying to gh-pages from @ VladyslavHn/CrazySuperStars@e300c469a844211e3a7841d08059346743508025 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,23 +1,23 @@
 import{b as o}from"./assets/api-12bdbdef.js";import"./assets/vendor-0cb09735.js";const g=async()=>{const e=document.querySelector(".bestsellers-list");try{const r=(await o.getBestSellers()).map(({books:s,list_name:a})=>`<li class="bestsellers-item">
              <h2 class="bestsellers-category-title">${a}</h2>
                 <ul class="bestsellers-books-list"> 
-                    ${s.map(({book_image:c,title:l,author:n})=>`<li class="bestsellers-books-item">
+                    ${s.map(({book_image:c,title:l,author:i})=>`<li class="bestsellers-books-item">
                             <img class="bestsellers-books-img" src='${c}' alt="" />
                             <h3 class="bestsellers-book-title">${l}</h3>
-                            <p class="bestsellers-book-author">${n}</p>
+                            <p class="bestsellers-book-author">${i}</p>
                         </li>`).join(`
 `)}
                 </ul>
                 <button class="bestsellers-btn" type="button">See more</button>
             </li>`).join(`
-`);e.insertAdjacentHTML("beforeend",r)}catch(t){console.error("Error fetching best sellers:",t)}};g();function y(e,t){const r=document.querySelector(".bestsellers-list"),s=document.querySelector(".bestsellers-title");s.textContent=t;const a=e.map(({author:c,book_image:l,title:n})=>`<li class="book-category-item">
+`);e.insertAdjacentHTML("beforeend",r)}catch(t){console.error("Error fetching best sellers:",t)}};g();function y(e,t){const r=document.querySelector(".bestsellers-list"),s=document.querySelector(".bestsellers-title");s.textContent=t;const a=e.map(({author:c,book_image:l,title:i})=>`<li class="book-category-item">
   <div class="book-category-card">
     <img class="book-category-image" src="${l}" alt="Book cover" width="180" />
     <div class="book-category-text">
       <h3 class = "book-title">
-      ${n}</h3>
+      ${i}</h3>
       <p class = "author-name"> ${c}</p>
     </div>
   </div>
-</li>`).join("");r.insertAdjacentHTML("beforeend",a)}const i={categoryContainer:document.querySelector(".sidebar-category-container"),categoryList:document.querySelector(".sidebar-category-list"),allCategory:document.querySelector(".all-category")};function b(e){return e.map(r=>`<li class='sidebar-category-item' data-source="${r.list_name}">${r.list_name}</li>`).join("")}(async()=>{try{const e=await o.getCategoryList(),t=b(e);i.categoryList.insertAdjacentHTML("beforeend",t)}catch(e){console.log(e)}})();i.allCategory.addEventListener("click",async()=>{try{const e=await o.getBestSellers();g(e)}catch(e){console.log(e)}});i.categoryList.addEventListener("click",async e=>{if(e.target.classList.contains("sidebar-category-item")){const t=e.target.dataset.source;document.querySelectorAll(".sidebar-category-item").forEach(s=>{s.classList.remove("category-active")}),e.target.classList.add("category-active");try{const s=await o.getSelectedCategory(t);y(s)}catch(s){console.log(s)}}});
+</li>`).join("");r.innerHTML=a}const n={categoryContainer:document.querySelector(".sidebar-category-container"),categoryList:document.querySelector(".sidebar-category-list"),allCategory:document.querySelector(".all-category")};function b(e){return e.map(r=>`<li class='sidebar-category-item' data-source="${r.list_name}">${r.list_name}</li>`).join("")}(async()=>{try{const e=await o.getCategoryList(),t=b(e);n.categoryList.insertAdjacentHTML("beforeend",t)}catch(e){console.log(e)}})();n.allCategory.addEventListener("click",async()=>{try{const e=await o.getBestSellers();g(e)}catch(e){console.log(e)}});n.categoryList.addEventListener("click",async e=>{if(e.target.classList.contains("sidebar-category-item")){const t=e.target.dataset.source;document.querySelectorAll(".sidebar-category-item").forEach(s=>{s.classList.remove("category-active")}),e.target.classList.add("category-active");try{const s=await o.getSelectedCategory(t);y(s)}catch(s){console.log(s)}}});
 //# sourceMappingURL=commonHelpers2.js.map
