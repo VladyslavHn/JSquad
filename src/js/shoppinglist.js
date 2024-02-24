da95d5f249e71c19e43c731b7a40a5240a8078c2
dmvoytyuk <dmvoytyuk@gmail.com>
2024-02-24 14:17:53 +0100

    li for book map

--- src/js/shoppinglist.js
+++ src/js/shoppinglist.js
@@ -1,3 +1,43 @@
 import { renderTitle } from '../js/Services/helpers';
+import localStorageBooks from '../js/localstorage';
+
+const shoppingList = document.querySelector('.cart-list');
+const cartEmptyMsg = document.querySelector('.cart-empty');
 
 renderTitle('.container', 'Shopping List');
+
+function renderShoppingList(books) {
+  console.log(books);
+  shoppingList.innerHTML = '';
+  const hmtlBookList = books
+    .map(book => {
+      return `<li>${book.title}</li>`;
+    })
+    .join('');
+  shoppingList.insertAdjacentHTML('beforeend', hmtlBookList);
+}
+
+function renderShoppingListPage() {
+  const books = localStorageBooks.getAllBooks();
+  if (books.legth === 0) {
+    cartEmptyMsg.classList.remove('is-hidden');
+  } else {
+    renderShoppingList(books);
+  }
+}
+
+// const book = {
+//   _id: '643282b1e85766588626a0ae',
+//   title: "A GOOD GIRL'S GUIDE TO MURDER",
+//   author: 'Holly Jackson',
+//   list_name: 'Series Books',
+//   book_image:
+//     'https://storage.googleapis.com/du-prd/books/images/9780593379851.jpg',
+//   description: 'Pippa Fitz-Amobi solves murderous crimes.',
+//   amazon_buy_link: 'https://www.amazon.com/dp/0593379853?tag=NYTBSREV-20',
+//   apple_buy_link: 'https://goto.applebooks.apple/9781984896391?at=10lIEQ',
+// };
+
+// localStorageBooks.addBookToFavorites(book);
+
+renderShoppingListPage();
