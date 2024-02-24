e56525eb3d2c3befb16583cfc189a9c9b4f39468
VladyslavHn <141771169+VladyslavHn@users.noreply.github.com>
2024-02-24 02:41:29 +0000

    Deploying to gh-pages from @ VladyslavHn/CrazySuperStars@9c0e67eee9d8bfda269ffb073a19120df11d409e 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,13 @@
-import{b as o}from"./assets/api-3310354d.js";import"./assets/vendor-0cb09735.js";renderBooks();const c={categoryContainer:document.querySelector(".category-container"),categoryList:document.querySelector(".category-list"),categoryItem:document.querySelector(".category-item"),allCategory:document.querySelector(".all-category")};function a(e){return e.map(r=>`<li class='category-item'>${r.list_name}</li>`).join("")}(async()=>{try{const e=await o.getCategoryList(),t=a(e);c.categoryList.insertAdjacentHTML("beforeend",t)}catch(e){console.log(e)}})();
+import{b as r}from"./assets/api-3310354d.js";import"./assets/vendor-0cb09735.js";const i=async()=>{const e=document.querySelector(".best-books");try{const s=(await r.getBestSellers()).map(({books:o,list_name:l})=>`<li class="bestsellers-item">${l}
+                <ul class="bestsellers-books-list"> 
+                    ${o.map(({book_image:c,title:a,author:n})=>`<li class="bestsellers-books-item">
+                            <img class="bestsellers-books-img" src='${c}' alt="" />
+                            <h3 class="bestsellers-book-title">${a}</h3>
+                            <p class="bestsellers-book-author">${n}</p>
+                        </li>`).join(`
+`)}
+                </ul>
+                <button class="bestsellers-btn" type="button">See more</button>
+            </li>`).join(`
+`);e.insertAdjacentHTML("beforeend",s)}catch(t){console.error("Error fetching best sellers:",t)}};i();const b={categoryContainer:document.querySelector(".category-container"),categoryList:document.querySelector(".category-list"),categoryItem:document.querySelector(".category-item"),allCategory:document.querySelector(".all-category")};function u(e){return e.map(s=>`<li class='category-item'>${s.list_name}</li>`).join("")}(async()=>{try{const e=await r.getCategoryList(),t=u(e);b.categoryList.insertAdjacentHTML("beforeend",t)}catch(e){console.log(e)}})();
 //# sourceMappingURL=commonHelpers2.js.map
