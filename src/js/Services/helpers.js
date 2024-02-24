eb536e5fcb62c6e4600a19570ba45e7f7fddc444
dmvoytyuk <dmvoytyuk@gmail.com>
2024-02-24 01:36:44 +0100

    render title function helper

---
+++ src/js/Services/helpers.js
@@ -0,0 +1,11 @@
+export function renderTitle(containerClass, title) {
+  const container = document.querySelector(con);
+  const words = title.split(' ');
+  const lastWord = words[words.length - 1];
+  words.pop();
+  title = words.join(' ');
+  container.insertAdjacentHTML(
+    'afterbegin',
+    `<h1 class="page-title">${title} <span class="page-title-highlight">${lastWord}</span></h1>`
+  );
+}
