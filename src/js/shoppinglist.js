37afc9ec4284c9fe06b3d5fe7451ea9cde55d937
dmvoytyuk <dmvoytyuk@gmail.com>
2024-02-24 18:13:02 +0100

    shopping list

--- src/js/shoppinglist.js
+++ src/js/shoppinglist.js
@@ -11,7 +11,56 @@ function renderShoppingList(books) {
   shoppingList.innerHTML = '';
   const hmtlBookList = books
     .map(book => {
-      return `<li>${book.title}</li>`;
+      return `<li class="cart-item data-id="${book._id}">
+      <img
+        class="cart-item-img"
+        src="${book.book_image}"
+        alt="book image"
+        width="100"
+        height="142"
+      />
+      <div class="cart-item-content">
+        <div class="cart-item-content-top">
+          <div class="cart-item-content-top-left">
+            <h3 class="cart-item-title">${book.title}</h3>
+            <p class="cart-item-category">${book.list_name}</p>
+          </div>
+          <button class="cart-item-del-button">
+            <svg class="cart-item-del-button-icon" width="12" height="12">
+              <use href="../img/symbol-defs.svg#icon-delete-shoppinglist-tab" />
+            </svg>
+          </button>
+        </div>
+        <div class="cart-item-content-middle">
+          <p class="cart-item-content-desc">
+            ${book.description}
+          </p>
+          <div class="cart-item-content-bottom">
+            <p class="cart-item-author">${book.author}</p>
+            <div class="cart-items-links">
+              <a
+                class="cart-items-link"
+                target="_blank"
+                href="${book.amazon_buy_link}"
+              >
+                <svg class="cart-item-amazon" width="32" height="11">
+                  <use href="../img/symbol-defs.svg#icon-amazon" />
+                </svg>
+              </a>
+              <a
+                class="cart-items-link"
+                target="_blank"
+                href="${book.apple_buy_link}"
+              >
+                <svg class="cart-items-apple" width="16" height="16">
+                  <use href="../img/symbol-defs.svg#icon-applebook" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>`;
     })
     .join('');
   shoppingList.insertAdjacentHTML('beforeend', hmtlBookList);
@@ -27,7 +76,7 @@ function renderShoppingListPage() {
 }
 
 // const book = {
-//   _id: '643282b1e85766588626a0ae',
+//   _id: '643282b1e85766588626a016',
 //   title: "A GOOD GIRL'S GUIDE TO MURDER",
 //   author: 'Holly Jackson',
 //   list_name: 'Series Books',
