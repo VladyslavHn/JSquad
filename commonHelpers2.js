9fa2366cb96d801b1b300dc155f708ededfe2922
VladyslavHn <141771169+VladyslavHn@users.noreply.github.com>
2024-02-25 11:34:39 +0000

    Deploying to gh-pages from @ VladyslavHn/CrazySuperStars@42b202585d10fa9cfd887b83e3b4ae8bc9968451 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{r as p,b as d,l as u}from"./assets/api-364cd347.js";import"./assets/vendor-0cb09735.js";function k({book_image:t,title:o,author:s}){return`<li class="book-category-item">
+import{r as p,b as d,l as u}from"./assets/api-cef72c98.js";import"./assets/vendor-0cb09735.js";function k({book_image:t,title:o,author:s}){return`<li class="book-category-item">
   <div class="book-category-card">
     <img class="book-category-image" src="${t}" alt="Book cover" width="180" />
     <div class="book-category-text">
