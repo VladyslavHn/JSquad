290ade59eb621a1c5dfce771dc153092804b9bd4
dmvoytyuk <dmvoytyuk@gmail.com>
2024-02-26 11:47:34 +0100

    Update shoppinglist.js

--- src/js/shoppinglist.js
+++ src/js/shoppinglist.js
@@ -1,16 +1,16 @@
 import { renderTitle } from '../js/Services/helpers';
 import localStorageBooks from '../js/localstorage';
-(function () {
-  const shoppingList = document.querySelector('.cart-list');
-  const cartEmptyMsg = document.querySelector('.cart-empty-container');
-  const paginationList = document.querySelector('.pagination-list');
 
-  renderTitle('.shoppinglist-title', 'Shopping List');
-  function renderShoppingList(books) {
-    shoppingList.innerHTML = '';
-    const hmtlBookList = books
-      .map(book => {
-        return `<li class="cart-item">
+const shoppingList = document.querySelector('.cart-list');
+const cartEmptyMsg = document.querySelector('.cart-empty-container');
+const paginationList = document.querySelector('.pagination-list');
+
+renderTitle('.shoppinglist-title', 'Shopping List');
+function renderShoppingList(books) {
+  shoppingList.innerHTML = '';
+  const hmtlBookList = books
+    .map(book => {
+      return `<li class="cart-item">
       <img
         class="cart-item-img"
         src="${book.book_image}"
@@ -60,244 +60,240 @@ import localStorageBooks from '../js/localstorage';
         </div>
       </div>
     </li>`;
-      })
-      .join('');
-    shoppingList.insertAdjacentHTML('beforeend', hmtlBookList);
+    })
+    .join('');
+  shoppingList.insertAdjacentHTML('beforeend', hmtlBookList);
+}
+
+let pageData = {
+  books: [],
+  currentPage: 1,
+  nextPage: 0,
+  prevPage: 0,
+  totalBooks: 0,
+  totalPages: 0,
+  booksPerPage: 0,
+};
+
+function renderShoppingListPage(pageData) {
+  paginationList.removeEventListener('click', buttonClickHandler);
+  shoppingList.removeEventListener('click', removeBookFromList);
+  const books = localStorageBooks.getAllBooks();
+  console.log(shoppingList);
+  shoppingList.innerHTML = '';
+  paginationList.innerHTML = '';
+  pageData.totalBooks = books.length;
+  pageData.booksPerPage = getBooksPerPage();
+  pageData.totalPages = getTotalPages(
+    pageData.totalBooks,
+    pageData.booksPerPage
+  );
+  pageData.books = createBooksListsPerPage(
+    books,
+    pageData.booksPerPage,
+    pageData.totalPages
+  );
+
+  console.log(pageData);
+
+  if (pageData.totalPages === 1) {
+    renderShoppingList(pageData.books[0]);
   }
 
-  let pageData = {
-    books: [],
-    currentPage: 1,
-    nextPage: 0,
-    prevPage: 0,
-    totalBooks: 0,
-    totalPages: 0,
-    booksPerPage: 0,
-  };
-
-  function renderShoppingListPage(pageData) {
-    paginationList.removeEventListener('click', buttonClickHandler);
-    shoppingList.removeEventListener('click', removeBookFromList);
-    const books = localStorageBooks.getAllBooks();
-    console.log(shoppingList);
-    shoppingList.innerHTML = '';
-    paginationList.innerHTML = '';
-    pageData.totalBooks = books.length;
-    pageData.booksPerPage = getBooksPerPage();
-    pageData.totalPages = getTotalPages(
-      pageData.totalBooks,
-      pageData.booksPerPage
-    );
-    pageData.books = createBooksListsPerPage(
-      books,
-      pageData.booksPerPage,
-      pageData.totalPages
-    );
-
-    console.log(pageData);
-
-    if (pageData.totalPages === 1) {
-      renderShoppingList(pageData.books[0]);
-    }
-
-    if (pageData.totalPages > 1) {
-      renderShoppingList(pageData.books[pageData.currentPage - 1]);
-      renderPaginationButtons(pageData.totalPages, paginationList);
-      makeActiveButton(pageData.currentPage);
-      paginationList.addEventListener('click', buttonClickHandler);
-    }
-    shoppingList.addEventListener('click', removeBookFromList);
+  if (pageData.totalPages > 1) {
+    renderShoppingList(pageData.books[pageData.currentPage - 1]);
+    renderPaginationButtons(pageData.totalPages, paginationList);
+    makeActiveButton(pageData.currentPage);
+    paginationList.addEventListener('click', buttonClickHandler);
   }
+  shoppingList.addEventListener('click', removeBookFromList);
+}
 
-  function buttonClickHandler(e) {
-    const clickedElement = e.target.closest('button');
-    if (clickedElement) {
-      const pageButton = clickedElement.dataset.page;
-      if (pageButton) {
-        renderShoppingList(pageData.books[pageButton - 1]);
-        makeActiveButton(pageButton);
-        pageData.currentPage = pageButton;
-        console.log('premuto pulsante: ', pageData);
-      }
-    }
-  }
-
-  function removeBookFromList(e) {
-    if (e.target.closest('.cart-item-del-button') !== null) {
-      const bookId = e.target.closest('.cart-item-del-button').dataset.id;
-      localStorageBooks.removeBookFromFavorites(bookId);
-      console.log(
-        'dentro listener: ',
-        pageData.books[pageData.currentPage - 1]
-      );
-      console.log('current page: ', pageData.currentPage);
-      console.log('totale page:', pageData.totalPages);
-      console.log(pageData.books[pageData.currentPage - 1].length);
-      if (
-        parseInt(pageData.currentPage) === pageData.totalPages &&
-        pageData.books[pageData.currentPage - 1].length === 1
-      ) {
-        console.log('eliminato ultimo libro della pagina ');
-        pageData.currentPage -= 1;
-        console.log(pageData.currentPage);
-      }
-      renderShoppingListPage(pageData);
+function buttonClickHandler(e) {
+  const clickedElement = e.target.closest('button');
+  if (clickedElement) {
+    const pageButton = clickedElement.dataset.page;
+    if (pageButton) {
+      renderShoppingList(pageData.books[pageButton - 1]);
+      makeActiveButton(pageButton);
+      pageData.currentPage = pageButton;
+      console.log('premuto pulsante: ', pageData);
     }
   }
-  //   const pagesBook = createBooksListsPerPage(books, booksPerPage, totalPages);
+}
 
-  //   // console.log('array for each page:', pagesBook);
-
-  //   if (totalBooks === 0) {
-  //     cartEmptyMsg.classList.remove('is-hidden');
-  //   } else {
-  //     if (totalPages > 1) {
-  //       console.log('current page: ', currentPage);
-  //       renderShoppingList(pagesBook[currentPage - 1]);
-  //       paginationButtons.addEventListener('click', buttonsHandler);
-  //       renderPaginationButtons(totalPages, paginationButtons);
-  //     }
-  //     // createCartPagination(pagesBook, paginationButtons, totalPages, currentPage);
-  //     shoppingList.addEventListener('click', remove);
-  //
-  //   }
-  // }
-
-  function getBooksPerPage() {
-    const windowWidth = window.innerWidth;
-    if (windowWidth < 767) {
-      return 4;
-    } else {
-      return 3;
+function removeBookFromList(e) {
+  if (e.target.closest('.cart-item-del-button') !== null) {
+    const bookId = e.target.closest('.cart-item-del-button').dataset.id;
+    localStorageBooks.removeBookFromFavorites(bookId);
+    console.log('dentro listener: ', pageData.books[pageData.currentPage - 1]);
+    console.log('current page: ', pageData.currentPage);
+    console.log('totale page:', pageData.totalPages);
+    console.log(pageData.books[pageData.currentPage - 1].length);
+    if (
+      parseInt(pageData.currentPage) === pageData.totalPages &&
+      pageData.books[pageData.currentPage - 1].length === 1
+    ) {
+      console.log('eliminato ultimo libro della pagina ');
+      pageData.currentPage -= 1;
+      console.log(pageData.currentPage);
     }
+    renderShoppingListPage(pageData);
   }
+}
+//   const pagesBook = createBooksListsPerPage(books, booksPerPage, totalPages);
 
-  function getTotalPages(totalBooks, booksPerPage) {
-    return Math.ceil(totalBooks / booksPerPage);
+//   // console.log('array for each page:', pagesBook);
+
+//   if (totalBooks === 0) {
+//     cartEmptyMsg.classList.remove('is-hidden');
+//   } else {
+//     if (totalPages > 1) {
+//       console.log('current page: ', currentPage);
+//       renderShoppingList(pagesBook[currentPage - 1]);
+//       paginationButtons.addEventListener('click', buttonsHandler);
+//       renderPaginationButtons(totalPages, paginationButtons);
+//     }
+//     // createCartPagination(pagesBook, paginationButtons, totalPages, currentPage);
+//     shoppingList.addEventListener('click', remove);
+//
+//   }
+// }
+
+function getBooksPerPage() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth < 767) {
+    return 4;
+  } else {
+    return 3;
   }
+}
 
-  function renderPaginationButtons(totalPages, paginationList) {
-    paginationList.innerHTML = '';
-    if (totalPages > 1) {
-      paginationList.insertAdjacentHTML(
-        'beforeend',
-        `<button class="pagination-buttons-first"><<</button>
+function getTotalPages(totalBooks, booksPerPage) {
+  return Math.ceil(totalBooks / booksPerPage);
+}
+
+function renderPaginationButtons(totalPages, paginationList) {
+  paginationList.innerHTML = '';
+  if (totalPages > 1) {
+    paginationList.insertAdjacentHTML(
+      'beforeend',
+      `<button class="pagination-buttons-first"><<</button>
       <button class="pagination-buttons-prev"><</button>
       <button class="pagination-buttons-next">></button>
       <button class="pagination-buttons-last">>></button>`
-      );
+    );
 
-      const prevButton = document.querySelector('.pagination-buttons-prev');
-      prevButton.insertAdjacentHTML(
-        'afterend',
-        '<ul class="pagination-buttons-numbers"></ul>'
-      );
+    const prevButton = document.querySelector('.pagination-buttons-prev');
+    prevButton.insertAdjacentHTML(
+      'afterend',
+      '<ul class="pagination-buttons-numbers"></ul>'
+    );
 
-      const numbersButtonsList = document.querySelector(
-        '.pagination-buttons-numbers'
-      );
+    const numbersButtonsList = document.querySelector(
+      '.pagination-buttons-numbers'
+    );
 
-      for (let i = 0; i < totalPages; i++) {
-        const pageNumber = i + 1;
-        numbersButtonsList.insertAdjacentHTML(
-          'beforeend',
-          `<li><button data-page="${pageNumber}">${pageNumber}</button></li>`
-        );
-      }
-    }
-  }
-
-  // function buttonsHandler(e) {
-  //   if (e.target.closest('button')) {
-  //     if (e.target.closest('button').dataset.page) {
-  //       console.log(e.target.closest('button').dataset.page);
-  //       let newCurrentPage = e.target.closest('button').dataset.page;
-  //       let currentButton = document.querySelector('.active');
-  //       if (currentButton !== null) {
-  //         currentButton.classList.remove('active');
-  //       }
-  //       makeActiveButton(newCurrentPage);
-  //       currentButton = document.querySelector('.active');
-  //       console.log(currentButton);
-  //       renderShoppingList(pagesBook[newCurrentPage - 1]);
-  //     }
-  //     // if (
-  //     //   e.target
-  //     //     .closest('button')
-  //     //     .classList.contains('pagination-buttons-prev')
-  //     // ) {
-  //     //   const currentButton = document.querySelector('.active');
-  //     //   if (currentButton !== null) {
-  //     //     const currentPage = currentButton.dataset.page;
-  //     //     console.log('pressed button prev, current page: ', currentPage);
-  //     //     if (currentPage > 1) {
-  //     //       renderShoppingList(books[currentPage - 2]);
-  //     //       currentButton.classList.remove('active');
-  //     //       console.log('prev', currentButton, books);
-  //     //       makeActiveButton(parseInt(currentPage) - 1);
-  //     //     }
-  //     //   }
-  //     // }
-  //     // if (
-  //     //   e.target
-  //     //     .closest('button')
-  //     //     .classList.contains('pagination-buttons-next')
-  //     // ) {
-  //     //   const currentButton = document.querySelector('.active');
-  //     //   if (currentButton !== null) {
-  //     //     const currentPage = currentButton.dataset.page;
-  //     //     console.log(currentPage);
-  //     //     if (currentPage < books.length) {
-  //     //       console.log('pressed button next, current page: ', currentPage);
-  //     //       renderShoppingList(books[currentPage]);
-  //     //       currentButton.classList.remove('active');
-  //     //       console.log('next', currentButton, books);
-  //     //       makeActiveButton(parseInt(currentPage) + 1);
-  //     //     }
-  //     //   }
-  //     // }
-  //   }
-  // }
-
-  function makeActiveButton(currentPage) {
-    console.log('make active button with page ', currentPage);
-    const currentButton = document.querySelector('.active');
-    currentButton?.classList.remove('active');
-    const activeButton = document.querySelector(`[data-page="${currentPage}"]`);
-    activeButton.classList.add('active');
-  }
-
-  function createBooksListsPerPage(books, booksPerPage, totalPages) {
-    let booksPages = [];
     for (let i = 0; i < totalPages; i++) {
-      booksPages[i] = books.splice(0, booksPerPage);
+      const pageNumber = i + 1;
+      numbersButtonsList.insertAdjacentHTML(
+        'beforeend',
+        `<li><button data-page="${pageNumber}">${pageNumber}</button></li>`
+      );
     }
-    return booksPages;
   }
+}
 
-  // for (let i = 0; i < 20; i++) {
-  //   const book = {
-  //     _id: `${i}`,
-  //     title: `${i}`,
-  //     author: 'Michelle Zauner',
-  //     list_name: 'Paperback Nonfiction',
-  //     book_image:
-  //       'https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg',
-  //     description:
-  //       'The daughter of a Korean mother and Jewish American father, and leader of the indie rock project Japanese Breakfast, describes creating her own identity after losing her mother to cancer.',
-  //     amazon_buy_link: 'https://www.amazon.com/dp/0593379853?tag=NYTBSREV-20',
-  //     apple_buy_link: 'https://goto.applebooks.apple/9781984896391?at=10lIEQ',
-  //   };
+// function buttonsHandler(e) {
+//   if (e.target.closest('button')) {
+//     if (e.target.closest('button').dataset.page) {
+//       console.log(e.target.closest('button').dataset.page);
+//       let newCurrentPage = e.target.closest('button').dataset.page;
+//       let currentButton = document.querySelector('.active');
+//       if (currentButton !== null) {
+//         currentButton.classList.remove('active');
+//       }
+//       makeActiveButton(newCurrentPage);
+//       currentButton = document.querySelector('.active');
+//       console.log(currentButton);
+//       renderShoppingList(pagesBook[newCurrentPage - 1]);
+//     }
+//     // if (
+//     //   e.target
+//     //     .closest('button')
+//     //     .classList.contains('pagination-buttons-prev')
+//     // ) {
+//     //   const currentButton = document.querySelector('.active');
+//     //   if (currentButton !== null) {
+//     //     const currentPage = currentButton.dataset.page;
+//     //     console.log('pressed button prev, current page: ', currentPage);
+//     //     if (currentPage > 1) {
+//     //       renderShoppingList(books[currentPage - 2]);
+//     //       currentButton.classList.remove('active');
+//     //       console.log('prev', currentButton, books);
+//     //       makeActiveButton(parseInt(currentPage) - 1);
+//     //     }
+//     //   }
+//     // }
+//     // if (
+//     //   e.target
+//     //     .closest('button')
+//     //     .classList.contains('pagination-buttons-next')
+//     // ) {
+//     //   const currentButton = document.querySelector('.active');
+//     //   if (currentButton !== null) {
+//     //     const currentPage = currentButton.dataset.page;
+//     //     console.log(currentPage);
+//     //     if (currentPage < books.length) {
+//     //       console.log('pressed button next, current page: ', currentPage);
+//     //       renderShoppingList(books[currentPage]);
+//     //       currentButton.classList.remove('active');
+//     //       console.log('next', currentButton, books);
+//     //       makeActiveButton(parseInt(currentPage) + 1);
+//     //     }
+//     //   }
+//     // }
+//   }
+// }
 
-  //   localStorageBooks.addBookToFavorites(book);
-  // }
+function makeActiveButton(currentPage) {
+  console.log('make active button with page ', currentPage);
+  const currentButton = document.querySelector('.active');
+  currentButton?.classList.remove('active');
+  const activeButton = document.querySelector(`[data-page="${currentPage}"]`);
+  activeButton.classList.add('active');
+}
 
-  renderShoppingListPage(pageData);
+function createBooksListsPerPage(books, booksPerPage, totalPages) {
+  let booksPages = [];
+  for (let i = 0; i < totalPages; i++) {
+    booksPages[i] = books.splice(0, booksPerPage);
+  }
+  return booksPages;
+}
 
-  //   let variabileLocale = 'Valore';
+// for (let i = 0; i < 20; i++) {
+//   const book = {
+//     _id: `${i}`,
+//     title: `${i}`,
+//     author: 'Michelle Zauner',
+//     list_name: 'Paperback Nonfiction',
+//     book_image:
+//       'https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg',
+//     description:
+//       'The daughter of a Korean mother and Jewish American father, and leader of the indie rock project Japanese Breakfast, describes creating her own identity after losing her mother to cancer.',
+//     amazon_buy_link: 'https://www.amazon.com/dp/0593379853?tag=NYTBSREV-20',
+//     apple_buy_link: 'https://goto.applebooks.apple/9781984896391?at=10lIEQ',
+//   };
 
-  //   document.getElementById('mioElemento').addEventListener('click', function () {
-  //     // Usa variabileLocale qui
-  //     console.log(variabileLocale);
-  //   });
-})();
+//   localStorageBooks.addBookToFavorites(book);
+// }
+
+renderShoppingListPage(pageData);
+
+//   let variabileLocale = 'Valore';
+
+//   document.getElementById('mioElemento').addEventListener('click', function () {
+//     // Usa variabileLocale qui
+//     console.log(variabileLocale);
+//   });
