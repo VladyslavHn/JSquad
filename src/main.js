48086f38f9c806908be9d71019e4d5a01c3832a8
VladyslavHn <vlad.gnatok@gmail.com>
2024-02-26 04:19:04 +0100

    scrollup

--- src/main.js
+++ src/main.js
@@ -13,3 +13,27 @@ import './js/localstorage'
 import { topPageBestsellersBooks } from './js/bestsellers';
 
 topPageBestsellersBooks();
+
+document.addEventListener("DOMContentLoaded", function() {
+  const scrollUpButton = document.querySelector('.scroll-up');
+
+  scrollUpButton.classList.remove('show');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 650) {
+      scrollUpButton.classList.add('show');
+      // scrollUpButton.classList.add('border');
+    } else {
+      scrollUpButton.classList.remove('show');
+      // scrollUpButton.classList.remove('border');
+    }
+  });
+
+  scrollUpButton.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+    // scrollUpButton.classList.remove('.scroll-up.border');
+  });
+});
