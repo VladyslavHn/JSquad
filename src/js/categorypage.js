5ca52dc87e341fbf4f420cb8b8b52f4a123a8907
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-02-27 00:37:59 +0200

    notification

--- src/js/categorypage.js
+++ src/js/categorypage.js
@@ -4,29 +4,49 @@ import { renderTitle, bookTemplate } from './Services/helpers';
 import { renderModal, showModal } from './modalwindow';
 
 export function renderCategoryPage(books, category) {
-  showLoader()
-  try {
-  const pageContainer = document.querySelector('.bestsellers-container');
-  pageContainer.innerHTML = '';
-  renderTitle('.bestsellers-container', category);
-  pageContainer.insertAdjacentHTML(
-    'beforeend',
-    '<ul class = "book-category-list"></ul>'
-  );
-  const categoryPage = document.querySelector('.book-category-list');
-  const markup = books
-    .map(({ author, book_image, title, _id }) => {
-      return bookTemplate({ author, book_image, title, _id });
-    })
-    .join('');
-
-  categoryPage.innerHTML = markup;
-    categoryPage.addEventListener('click', onClickBook);
-  } catch {
-    console.log('Error fetching modal:', error);
-  } finally {
-    hideLoader();
+  showLoader();
+  if (books.length === 0) {
+    try {
+      const pageContainer = document.querySelector('.bestsellers-container');
+      pageContainer.innerHTML = '';
+      renderTitle('.bestsellers-container', category);
+      pageContainer.insertAdjacentHTML(
+        'beforeend',
+        '<ul class = "book-category-list"></ul>'
+      );
+      const categoryPage = document.querySelector('.book-category-list');
+      const markupEmpty =
+        '<p class = "category-text-empty">Sorry, no books were found for the given category</p>';
+      categoryPage.innerHTML = markupEmpty;
+    } catch {
+      console.log('Error fetching modal:', error);
+    } finally {
+      hideLoader();
     }
+  } else {
+    try {
+      const pageContainer = document.querySelector('.bestsellers-container');
+      pageContainer.innerHTML = '';
+      renderTitle('.bestsellers-container', category);
+      pageContainer.insertAdjacentHTML(
+        'beforeend',
+        '<ul class = "book-category-list"></ul>'
+      );
+      const categoryPage = document.querySelector('.book-category-list');
+      const markup = books
+        .map(({ author, book_image, title, _id }) => {
+          return bookTemplate({ author, book_image, title, _id });
+        })
+        .join('');
+
+      categoryPage.innerHTML = markup;
+      categoryPage.addEventListener('click', onClickBook);
+    } catch {
+      console.log('Error fetching modal:', error);
+    } finally {
+      hideLoader();
+    }
+  }
 }
 
 async function onClickBook(e) {
