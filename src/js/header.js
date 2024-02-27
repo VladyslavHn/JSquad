254eecec000c9199e0f2a954856289b0a9acef6b
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-02-27 15:32:51 +0100

    not-end

--- src/js/header.js
+++ src/js/header.js
@@ -1,33 +1,3 @@
-// Функція, яка видаляє клас 'exception' з усіх елементів меню, окрім клікнутого
-function handleMenuLinkClick(event, menuSelector, menuLinkSelector) {
-  if (event.target.classList.contains(menuLinkSelector)) {
-    event.preventDefault();
-    
-    const menu = document.querySelector(menuSelector);
-    const menuLinks = menu.querySelectorAll('.' + menuLinkSelector);
-
-    menuLinks.forEach(link => {
-      if (link !== event.target) {
-        link.classList.remove('exception');
-      }
-    });
-
-    event.target.classList.add('exception');
-
-    const path = event.target.getAttribute('href');
-    window.location.pathname = path;
-  }
-}
-
-document.querySelector('.header-menu').addEventListener('click', function(event) {
-  handleMenuLinkClick(event, '.header-menu', 'header-menu-link');
-});
-
-document.querySelector('.mob-list').addEventListener('click', function(event) {
-  handleMenuLinkClick(event, '.mob-list', 'mob-menu-link');
-});
-
-
 const themeSwitch = document.querySelector('.theme-switch-input');
 const body = document.querySelector('body');
 
@@ -43,7 +13,6 @@ function toggleTheme() {
         localStorage.setItem('theme', 'dark');
     }
 }
-
 
 // Перевірка, яка тема встановлена за замовчуванням
 const savedTheme = localStorage.getItem('theme');
@@ -63,12 +32,21 @@ const menuOpenButton = document.querySelector('.menu-open-btn');
 const menuCloseButton = document.querySelector('.menu-close-btn');
 const mobMenu = document.querySelector('.mob-menu');
 
-// Відкриття меню при кліку на кнопку відкриття
 menuOpenButton.addEventListener('click', () => {
   mobMenu.classList.add('is-open');
   menuOpenButton.classList.remove('is-open')
   menuOpenButton.classList.add('hidden');
   menuCloseButton.classList.remove('hidden');
+
+  // Перевірка, чи посиланням перейшли на сторінку списку покупок
+  const isShoppingListPage = window.location.pathname === '/card.html';
+  
+  // Додавання класу 'btn-active' до другого посилання, якщо ми на сторінці списку покупок
+  if (isShoppingListPage) {
+    document.querySelector('.mob-menu-calor').classList.add('btn-active');
+  } else {
+    document.querySelector('.mob-menu-link').classList.add('btn-active');
+  }
 });
 
 // Закриття меню при кліку на кнопку закриття
@@ -79,24 +57,89 @@ menuCloseButton.addEventListener('click', () => {
 });
 
 
+// варіант 1
+
+// Функція, яка видаляє клас 'btn-active' з усіх елементів меню, окрім клікнутого
+// function handleMenuLinkClick(event, menuSelector, menuLinkSelector) {
+//   if (event.target.classList.contains(menuLinkSelector)) {
+//     event.preventDefault();
+    
+//     const menu = document.querySelector(menuSelector);
+//     const menuLinks = menu.querySelectorAll('.' + menuLinkSelector);
+
+//     menuLinks.forEach(link => {
+//       if (link !== event.target) {
+//         link.classList.remove('btn-active');
+//       }
+//     });
+
+//     event.target.classList.add('btn-active');
+
+//     const path = event.target.getAttribute('href');
+//     window.location.pathname = path;
+//   }
+// }
+
+// document.querySelector('.header-menu').addEventListener('click', function(event) {
+//   handleMenuLinkClick(event, '.header-menu', 'header-menu-link');
+// });
+
+// document.querySelector('.mob-list').addEventListener('click', function(event) {
+//   handleMenuLinkClick(event, '.mob-list', 'mob-menu-link');
+// });
+
+
+
+// варіант 2
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const home = document.querySelector('.header-menu-home');
+//   const shoppingList = document.querySelector('.header-menu-shopping');
+
+//   shoppingList.addEventListener('click', onShopping);
+
+//   function onShopping() {
+//       home.classList.remove('btn-active');
+//       shoppingList.classList.add('btn-active');
+//     }
+
+//   home.addEventListener('click', onHome);
+
+//   function onHome() {
+//       shoppingList.classList.remove('btn-active');
+//       home.classList.add('btn-active');
+//   }
+// });
+
+
+
+// варіант 3
 
 document.addEventListener('DOMContentLoaded', function() {
   const home = document.querySelector('.header-menu-home');
   const shoppingList = document.querySelector('.header-menu-shopping');
 
-  shoppingList.addEventListener('click', onShopping);
-
-  function onShopping() {
-      home.classList.remove('btn-active');
-      shoppingList.classList.add('btn-active');
+  function handleShoppingClick() {
+    home.classList.toggle('btn-active');
+    shoppingList.classList.toggle('btn-active');
+    if (window.location.pathname === '/') {
+      window.location.pathname = '/card.html';
+    } else {
+      window.location.pathname = '/';
     }
-
-  home.addEventListener('click', onHome);
-
-  function onHome() {
-      shoppingList.classList.remove('btn-active');
-      home.classList.add('btn-active');
   }
+
+  shoppingList.addEventListener('click', handleShoppingClick);
+
+  function handleHomeClick() {
+    shoppingList.classList.toggle('btn-active');
+    home.classList.toggle('btn-active');
+    if (window.location.pathname === '/card.html') {
+      window.location.pathname = '/';
+    } else {
+      window.location.pathname = '/card.html';
+    }
+  }
+
+  home.addEventListener('click', handleHomeClick);
 });
-
-
