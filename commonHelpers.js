63a72e7a3cee20eaebb912bc706712b79bce98a9
VladyslavHn <141771169+VladyslavHn@users.noreply.github.com>
2024-02-28 10:29:18 +0000

    Deploying to gh-pages from @ VladyslavHn/CrazySuperStars@b3a09c3ec6dcea37efffbb7f25601c90d8d7b531 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{r as k,l as d}from"./assets/api-bbdc199a.js";import"./assets/vendor-8dea2054.js";const r=document.querySelector(".cart-list"),B=document.querySelector(".cart-empty-container"),s=document.querySelector(".pagination-list"),L=document.querySelector(".cart-buttons-container");k(".shoppinglist-title","Shopping List");function a(t){r.innerHTML="";const n=t.map(e=>`<li class="cart-item">
+import{r as k,l as d,a as B,b as L}from"./assets/api-a54d45ce.js";import"./assets/vendor-8dea2054.js";const y="/CrazySuperStars/assets/symbol-defs-8546edc5.svg",r=document.querySelector(".cart-list"),S=document.querySelector(".cart-empty-container"),s=document.querySelector(".pagination-list"),$=document.querySelector(".cart-buttons-container");k(".shoppinglist-title","Shopping List");function a(t){r.innerHTML="";const o=t.map(e=>`<li class="cart-item">
       <img
         class="cart-item-img"
         src="${e.book_image}"
@@ -15,7 +15,7 @@ import{r as k,l as d}from"./assets/api-bbdc199a.js";import"./assets/vendor-8dea2
           <button data-id="${e._id}" class="cart-item-del-button">
             <svg class="cart-item-del-button-icon" width="12" height="12">
 
-              <use href="./img/symbol-defs.svg#icon-delete-shoppinglist-tab" />
+              <use href="${y}#icon-delete-shoppinglist-tab" />
             </svg>
           </button>
         </div>
@@ -33,7 +33,7 @@ import{r as k,l as d}from"./assets/api-bbdc199a.js";import"./assets/vendor-8dea2
               >
                 <img
                   class="cart-items-amazon"
-                  src="./img/amazon-n.png"
+                  src="${B}"
                   alt="shopping cart empty"
                   height="11"
                   width="32"
@@ -47,7 +47,7 @@ import{r as k,l as d}from"./assets/api-bbdc199a.js";import"./assets/vendor-8dea2
                 <img
                   class="cart-items-apple"
 
-                  src="./img/apple-n.png"
+                  src="${L}"
                   alt="shopping cart empty"
                   height="16"
                   width="16"
@@ -57,5 +57,5 @@ import{r as k,l as d}from"./assets/api-bbdc199a.js";import"./assets/vendor-8dea2
           </div>
         </div>
       </div>
-    </li>`).join("");r.insertAdjacentHTML("beforeend",n)}let o={books:[],currentPage:0,nextPage:0,prevPage:0,totalBooks:0,totalPages:0,booksPerPage:0,buttonsPerPage:0,totalButtonsGroups:0,pagesOnLastGroup:0};function g(t){s.removeEventListener("click",l),r.removeEventListener("click",u);const n=d.getAllBooks();if(r.innerHTML="",s.innerHTML="",t.totalBooks=n.length,t.booksPerPage=S(),t.totalPages=$(t.totalBooks,t.booksPerPage),t.books=M(n,t.booksPerPage,t.totalPages),t.buttonsPerPage=w(),t.totalButtonsGroups=Math.ceil(t.totalPages/t.buttonsPerPage),t.pagesOnLastGroup=t.totalPages%t.totalButtonsGroups,t.totalBooks>0){if(t.totalPages===1&&a(t.books[0]),t.totalPages>1){a(t.books[t.currentPage]),y(t,s),p(t.currentPage);const e=document.querySelector(".active");m(e),s.addEventListener("click",l)}r.addEventListener("click",u)}else B.classList.remove("is-hidden")}function y(t,n){if(n.innerHTML="",t.totalPages>1)for(let e=0;e<t.totalPages;e++)n.insertAdjacentHTML("beforeend",`<li><button data-page="${e}">${e+1}</button></li>`)}function l(t){const n=t.target.closest("button");if(n){const e=n.dataset.page;e&&(a(o.books[e]),p(e),o.currentPage=e,m(n))}}function m(t){const n=t.getBoundingClientRect(),e=L.getBoundingClientRect(),c=s.getBoundingClientRect(),i=e.width/2,h=n.width/2,v=n.left,b=c.left,f=parseInt(i)-parseInt(v)-parseInt(h)+parseInt(b);s.style.translate=`${f}px 0`}function u(t){if(t.target.closest(".cart-item-del-button")!==null){const n=t.target.closest(".cart-item-del-button").dataset.id;d.removeBookFromFavorites(n),parseInt(o.currentPage)===parseInt(o.totalPages)-1&&o.books[o.currentPage].length===1&&o.currentPage>=1&&(o.currentPage-=1),g(o)}}function P(){return window.innerWidth<767}function S(){return P()?4:3}function w(){return P()?2:3}function $(t,n){return Math.ceil(t/n)}function p(t){const n=document.querySelector(".active");n==null||n.classList.remove("active"),document.querySelector(`[data-page="${t}"]`).classList.add("active")}function M(t,n,e){let c=[];for(let i=0;i<e;i++)c[i]=t.splice(0,n);return c}g(o);
+    </li>`).join("");r.insertAdjacentHTML("beforeend",o)}let n={books:[],currentPage:0,nextPage:0,prevPage:0,totalBooks:0,totalPages:0,booksPerPage:0,buttonsPerPage:0,totalButtonsGroups:0,pagesOnLastGroup:0};function m(t){s.removeEventListener("click",l),r.removeEventListener("click",u);const o=d.getAllBooks();if(r.innerHTML="",s.innerHTML="",t.totalBooks=o.length,t.booksPerPage=M(),t.totalPages=_(t.totalBooks,t.booksPerPage),t.books=C(o,t.booksPerPage,t.totalPages),t.buttonsPerPage=T(),t.totalButtonsGroups=Math.ceil(t.totalPages/t.buttonsPerPage),t.pagesOnLastGroup=t.totalPages%t.totalButtonsGroups,t.totalBooks>0){if(t.totalPages===1&&a(t.books[0]),t.totalPages>1){a(t.books[t.currentPage]),w(t,s),p(t.currentPage);const e=document.querySelector(".active");g(e),s.addEventListener("click",l)}r.addEventListener("click",u)}else S.classList.remove("is-hidden")}function w(t,o){if(o.innerHTML="",t.totalPages>1)for(let e=0;e<t.totalPages;e++)o.insertAdjacentHTML("beforeend",`<li><button data-page="${e}">${e+1}</button></li>`)}function l(t){const o=t.target.closest("button");if(o){const e=o.dataset.page;e&&(a(n.books[e]),p(e),n.currentPage=e,g(o))}}function g(t){const o=t.getBoundingClientRect(),e=$.getBoundingClientRect(),c=s.getBoundingClientRect(),i=e.width/2,h=o.width/2,b=o.left,v=c.left,f=parseInt(i)-parseInt(b)-parseInt(h)+parseInt(v);s.style.translate=`${f}px 0`}function u(t){if(t.target.closest(".cart-item-del-button")!==null){const o=t.target.closest(".cart-item-del-button").dataset.id;d.removeBookFromFavorites(o),parseInt(n.currentPage)===parseInt(n.totalPages)-1&&n.books[n.currentPage].length===1&&n.currentPage>=1&&(n.currentPage-=1),m(n)}}function P(){return window.innerWidth<767}function M(){return P()?4:3}function T(){return P()?2:3}function _(t,o){return Math.ceil(t/o)}function p(t){const o=document.querySelector(".active");o==null||o.classList.remove("active"),document.querySelector(`[data-page="${t}"]`).classList.add("active")}function C(t,o,e){let c=[];for(let i=0;i<e;i++)c[i]=t.splice(0,o);return c}m(n);
 //# sourceMappingURL=commonHelpers.js.map
