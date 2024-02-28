5ecb3aa83c6d322c0a95421cdb933d8e2e773b7c
VladyslavHn <141771169+VladyslavHn@users.noreply.github.com>
2024-02-28 10:29:47 +0000

    Deploying to gh-pages from @ VladyslavHn/CrazySuperStars@74166a4fa6745dc9e9cba02d731c25c5bf3fb3fe 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{r as k,l as d,a as B,b as L}from"./assets/api-a54d45ce.js";import"./assets/vendor-8dea2054.js";const y="/CrazySuperStars/assets/symbol-defs-8546edc5.svg",r=document.querySelector(".cart-list"),S=document.querySelector(".cart-empty-container"),s=document.querySelector(".pagination-list"),$=document.querySelector(".cart-buttons-container");k(".shoppinglist-title","Shopping List");function a(t){r.innerHTML="";const o=t.map(e=>`<li class="cart-item">
+import{r as k,l as d,a as B,b as L}from"./assets/api-7b055e39.js";import"./assets/vendor-8dea2054.js";const y="/CrazySuperStars/assets/symbol-defs-8546edc5.svg",r=document.querySelector(".cart-list"),S=document.querySelector(".cart-empty-container"),s=document.querySelector(".pagination-list"),$=document.querySelector(".cart-buttons-container");k(".shoppinglist-title","Shopping List");function a(t){r.innerHTML="";const o=t.map(e=>`<li class="cart-item">
       <img
         class="cart-item-img"
         src="${e.book_image}"
